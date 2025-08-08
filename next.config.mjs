96ff648a14184416582ef8e36aa2f1838c1022c4
nicbramble <56664866+nicbramble@users.noreply.github.com>
2025-08-07 23:18:49 -0400

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
-    serverActions: { allowedOrigins: ['*'] }
-  }
-};
-export default nextConfig;
